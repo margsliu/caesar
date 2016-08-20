4eeeb7b4812e6e4ad67cb7809b5227c2d40d53bd
Margaret Liu <margs.liu@gmail.com>
2016-08-20 14:43:58 -0500

    add HTML header and footer

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ import cgi
 from helpers import alphabet_position, rotate_character
 from caesar import encrypt, user_input_is_valid
 
+# HTML header and footer
+
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formations: Caesar</title>
+</head>
+<body>
+    <h1>Formations: Caesar</h1>
+"""
+page_footer = """
+</body>
+</html>
+"""
+
+
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
